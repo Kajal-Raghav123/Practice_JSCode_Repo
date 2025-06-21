ad89c709b8338b55d95436e6ed1cc0e516836d6f
Kajal-Raghav123 <kajalraghav9368@gmail.com>
2025-06-21 22:21:09 +0530

    updated the closures file

--- JS/closures.js
+++ JS/closures.js
@@ -1,9 +1,49 @@
+// function outer(){
+//     let name ='Kajal'
+//     function details(){
+//         console.log(name)
+//     }
+//     return details
+// }
+// const fun = outer()
+// fun()
+// function createCounter()
+// {
+//      let count = 0
+//     return function increase(){
+//         count++
+//         console.log(count)
+//     }
+// }
+// const fun = createCounter()
+// fun()
+// function makeCounter() {
+//   let count = 0;
+//   return function() {
+//     return count++;
+//   };
+// }
+
+// const counter = makeCounter();
+// console.log(counter()); // ?
+// console.log(counter()); // ?
+
+// let count = 0;
+// function mul() {
+//     return count++;
+//   };
+
+//   console.log(mul())
+// let count = 0;
+// let result = count++;
+// console.log(result); // 0
+// console.log(count); 
 function outer(){
-    let name ='Kajal'
-    function details(){
-        console.log(name)
+    let name = "JavaScript"
+    function inner(){
+        console.log(name);
     }
-    return details
+    return inner;
 }
-const fun = outer()
-fun()
+const funcCall = outer();
+funcCall();
